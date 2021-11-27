4a15acfc2e4ba068326752ce2ca5669a12d928c9
Ali Sasani <alisasanibab@gmail.com>
2021-11-27 14:41:19 +0330

    creating models

--- whale_tracker/api_app/models.py
+++ whale_tracker/api_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class TransactionModel(models.Model):
+    blockchain = models.CharField(max_length=200)
+    symbol = models.CharField(max_length=200)
+    # id = models.IntegerField(primary_key=True)
+    transaction_type = models.CharField(max_length=200)
+    hash = models.CharField(max_length=200)
