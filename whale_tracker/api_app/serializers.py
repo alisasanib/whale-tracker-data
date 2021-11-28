c0ab2dbfd9cc77ab7f3452af908d56e9c6cdece2
Ali Sasani <alisasanibab@gmail.com>
2021-11-29 00:59:13 +0330

    updating serializers

--- whale_tracker/api_app/serializers.py
+++ whale_tracker/api_app/serializers.py
@@ -7,7 +7,12 @@ class TransactionSerializer(serializers.ModelSerializer):
     symbol = serializers.CharField(max_length=200)
     transaction_type = serializers.CharField(max_length=200)
     hash = serializers.CharField(max_length=200)
-    # id = serializers.IntegerField(primary_key=True)
+    from_source = serializers.JSONField(encoder=None, decoder=None)
+    to_source = serializers.JSONField(encoder=None, decoder=None)
+    timestamp = serializers.IntegerField()
+    amount = serializers.IntegerField()
+    amount_usd = serializers.IntegerField()
+    transaction_count = serializers.IntegerField()
 
     class Meta:
         model = TransactionModel
