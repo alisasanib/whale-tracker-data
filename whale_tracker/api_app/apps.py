2b0c4cd2123a4550611340f94ffe6b208fed811b
Ali Sasani <alisasanibab@gmail.com>
2021-11-29 01:09:58 +0330

    setting schedule in app.py

--- whale_tracker/api_app/apps.py
+++ whale_tracker/api_app/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class ApiAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api_app'
+
+    def ready(self):
+        print("Starting Scheduler ...")
+        from .whale_scheduler import whale_updater
+        whale_updater.start()
